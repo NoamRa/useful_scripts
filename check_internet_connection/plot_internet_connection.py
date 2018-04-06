f1ecceeb9477580a26acebb46c4c6328f0206caf
NoamRa <noamraby@gmail.com>
2018-04-06 12:34:07 +0300

    plot raw timing as well as log10 (timing) stacked in one html

--- check_internet_connection/plot_internet_connection.py
+++ check_internet_connection/plot_internet_connection.py
@@ -4,38 +4,50 @@
 # generate a plot using data collected by check_internet_connection.py
 
 
-import numpy as np
-import pandas as pd
-import plotly.figure_factory as FF
 import plotly.graph_objs as go
-# import plotly.plotly as py
+from plotly import tools
+from plotly.offline import plot
 import json
-import plotly.offline as offline
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+import math
+import pandas as pd
+
 
 df = pd.read_csv('./conn_test.csv')
-df_tabe = FF.create_table(df)
-# print(json.dumps(df_tabe, indent=2))
 
-# generate html table
-# fig = plot(df_tabe)
+test_list = list(df)[2:] # header row without test-number and test-time
+
+# trace each colum into raw data and log10
+raw_data = []
+for test in test_list:
+    trace = go.Scatter( x = df['test time'], 
+                        y=df[test],
+                        name=test)
+    raw_data.append(trace)
+
+log_data = []
+for test in test_list:
+    trace = go.Scatter( x = df['test time'], 
+                        y=df[test].map(lambda x: math.log10(x)), # use log10 scale
+                        name=test + " log10")
+    log_data.append(trace)
+
+# stack plots in one page
+fig = tools.make_subplots(rows=4, cols=1)
+
+for trace_num in xrange(len(raw_data)-1):
+    fig.append_trace(log_data[trace_num], 1, 1) # all log data together 
+    fig.append_trace(raw_data[trace_num], trace_num+2, 1) # indevidual plot for each test
 
 
-trace = go.Scatter(x = df['test time'], y = df['8.8.8.8'],
-                  name='connection test')
-layout = go.Layout(title='connection test',
-                   plot_bgcolor='rgb(230, 230,230)', 
-                   showlegend=True)
+# plot to html
+plot(fig)
 
-fig = go.Figure(data=[trace], layout=layout)
 
-# plot in html
-offline.plot(fig)
-
-# plot and save png
-# offline.plot(fig, 
-#             image = 'png', 
-#             image_filename='conn_test',
-#             output_type='file', 
-#             image_width=800, image_height=600)
+# # plot and save png
+# plot(fig, 
+#     image = 'png', 
+#     image_filename='conn_test',
+#     output_type='file', 
+#     image_width=1280, image_height=720
+#       )
 
